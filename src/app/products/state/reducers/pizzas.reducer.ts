f6d9cbc168bf52ea9946b832c00cff5a1e78d324
m0t0r <vitaly.tkachenko@jobsity.com>
2020-02-16 12:06:03 -0500

    refactor: use pizza entities in pizzas reducer

--- src/app/products/state/reducers/pizzas.reducer.ts
+++ src/app/products/state/reducers/pizzas.reducer.ts
@@ -1,30 +1,34 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import { Pizza } from '../../models/pizza.model';
 import * as fromPizzaActions from '../actions/pizzas.actions';
 
 export const pizzasFeatureKey = 'pizzas';
 
-export interface PizzaState {
-  data: Pizza[];
+export interface PizzaState extends EntityState<Pizza> {
+  selectedPizzaId: number | null;
   loaded: boolean;
   loading: boolean;
 }
 
-export const initialState: PizzaState = {
-  data: [],
+export const adapter: EntityAdapter<Pizza> = createEntityAdapter<Pizza>();
+
+export const initialState: PizzaState = adapter.getInitialState({
+  selectedPizzaId: null,
   loaded: false,
   loading: false,
-};
+});
 
 const pizzaReducer = createReducer(
   initialState,
   on(fromPizzaActions.loadPizzas, state => ({ ...state, loading: true })),
-  on(fromPizzaActions.loadPizzasSuccess, (state, { pizzas }) => ({
-    ...state,
-    data: pizzas,
-    loaded: true,
-    loading: false,
-  })),
+  on(fromPizzaActions.loadPizzasSuccess, (state, { pizzas }) => {
+    return adapter.addAll(pizzas, {
+      ...state,
+      loaded: true,
+      loading: false,
+    });
+  }),
   on(fromPizzaActions.loadPizzasFailure, state => ({ ...state, loaded: false, loading: false })),
 );
 
@@ -32,6 +36,6 @@ export function reducer(state: PizzaState | undefined, action: Action) {
   return pizzaReducer(state, action);
 }
 
+export const { selectAll: getPizzas } = adapter.getSelectors();
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
